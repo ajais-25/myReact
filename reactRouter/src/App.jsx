bc23cf9a639dbcf8390b91abce1546caeba99617
AKSAR <akshatjaiswal2518@gmail.com>
2024-05-23 20:21:24 +0530

    react router done

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl">React Router</h1>
+    </>
+  );
+}
+
+export default App;
